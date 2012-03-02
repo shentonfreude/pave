13f9f63c507b2e3c103530161d26c161af197b67
Chris Shenton <chris@koansys.com>
2012-03-02 10:27:05 -0500

    Create Home page with link to projects and login form; needs CSRF

--- pave/urls.py
+++ pave/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.views.generic.simple import direct_to_template
+
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'pave.views.home', name='home'),
+    url(r'^$', direct_to_template, {'template': 'home.html'}, ), #name='home'),
+
     # url(r'^pave/', include('pave.foo.urls')),
     (r'^project/', include('project.urls')), 
 
