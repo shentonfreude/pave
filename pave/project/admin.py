2d947ad4f924e0311b714b2804afb30bcc1e48f5
Chris Shenton <chris@koansys.com>
2012-03-02 17:38:01 -0500

    Create models for Centers, Job Titles, etc, etc for M2M relations

--- pave/project/admin.py
+++ pave/project/admin.py
@@ -1,4 +1,9 @@
-from models import Project
+from models import Center, Status, GradeLevel, JobCode, Applicant, Project
 from django.contrib import admin
 
+admin.site.register(Center)
+admin.site.register(Status)
+admin.site.register(GradeLevel)
+admin.site.register(JobCode)
+admin.site.register(Applicant)
 admin.site.register(Project)
