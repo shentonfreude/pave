bc239b48146a393148a23d48a5b338edebe396e9
Chris Shenton <chris@koansys.com>
2012-03-04 10:03:02 -0500

    Make initial Browse list show only "recent" Approved, Closed projects

--- pave/project/urls.py
+++ pave/project/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.simple import direct_to_template
 from django.views.generic.list_detail import object_list, object_detail
 from django.views.generic.create_update import create_object
 from models import Project
-from views import search
+from views import search, browse
 
 info_dict = {
     'queryset' : Project.objects.all()
@@ -11,11 +11,10 @@ info_dict = {
 
 urlpatterns = patterns(
     '',
-    # do I need to wrap these in url() to get name=... to work?
-    url(r'^$',                     object_list, info_dict, name="projects"),
+    url(r'^$',                     browse, name="projects"),
+    url(r'^search/$',              search, name="search"),
     url(r'^(?P<object_id>\d+)/$',  object_detail, info_dict, name="details"),
     # url(r'^search/$',              direct_to_template, {'template': 'search.html'}, name="search"),
-    url(r'^search/$',              search, name="search"),
     url(r'^new/$',                 create_object, {'model': Project}), # For admins only
 )
 
--- pave/project/views.py
+++ pave/project/views.py
@@ -50,3 +50,22 @@ def search(request):
     return render_to_response('search.html',
                               {'form': form},
                               context_instance=RequestContext(request));
+
+
+def browse(request):
+    """Show 10 most recent Approved and 10 most recent Closed, with total count of each.
+    """
+    LIMIT = 2
+    approved = Project.objects.filter(status__name="Approved").order_by('announcement_closes').reverse()
+    closed = Project.objects.filter(status__name="Closed").order_by('announcement_closes').reverse()
+#            import pdb; pdb.set_trace()
+    return render_to_response('project/project_browse.html', # reduce, reuse, recycle
+                              {'limit': LIMIT,
+                               'approved': approved[0:LIMIT],
+                               'approved_num': len(approved),
+                               'closed' : closed[0:LIMIT],
+                               'closed_num': len(closed)
+                               },
+                              context_instance=RequestContext(request));
+
+
