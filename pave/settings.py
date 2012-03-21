6a5c1b9e13d16059d2ba39b555381b276a510406
Chris Shenton <chris@koansys.com>
2012-03-21 11:22:22 -0400

    Use host=/tmp with postgresql running as mortal user
    host='' has postgresql create unix socket in /var which mortal users
    can't write to.

--- pave/settings.py
+++ pave/settings.py
@@ -25,7 +25,7 @@ DATABASES = {
         'NAME': 'pave',
         'USER': 'pave',                      # Not used with sqlite3.
         'PASSWORD': 'PaveThePlanet',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '/tmp',                      # Need /tmp for Postgres running as my username; Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
