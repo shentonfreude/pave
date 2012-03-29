02f9cf5acaaa68c9d41bf75b031f9dadbb5704c2
Chris Shenton <chris@koansys.com>
2012-03-29 19:52:16 -0400

    Left nav now context-sensitive-able, new favicon, new what's new
    Block 'leftnav' now is content block you can rewrite in any template.
    Useful for making a block which gives in-page context-sensitive links,
    including to internal sections (with #anchorname).
    This has been done for the Browse page.
    
    Removing all footer-duplicate noise from leftnav makes it much more
    appealing when viewport is compressed. Actually more useful too.
    
    Used a stripes-on-highway image for a favicon, with up-and-left angle
    suggesting an upward trajectory. Makes the page much more eye-catching
    on browser tabs.
    
    Added a What's New page which gets its info from a
    whats_new_items.html page. This way it can be used in multiple places.
    Currently, the Home leftnav as well as the About page.

--- pave/views.py
+++ pave/views.py
@@ -1,2 +1,9 @@
-def home():
-    # show a home screen
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def home(request):
+    # Manager login form needs CSRF context
+    return render_to_response('home.html',
+                              {},
+                              context_instance=RequestContext(request));
+
