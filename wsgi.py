ed2a9f4a88e30ffe9adc334548ce54350c0c6bd5
Chris Shenton <chris@koansys.com>
2012-04-07 15:42:25 -0400

    Incorrect .wsgi module name, fixed, no help :-(

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'hellodjango.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pave.settings'
 
 import django.core.handlers.wsgi as w
 application = w.WSGIHandler()
